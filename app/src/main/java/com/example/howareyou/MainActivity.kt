96863ec2f9c5e364ba220685c27664684c70e6b4
yeseopLee <yeseoplee94@gmail.com>
2020-10-06 19:17:04 +0900

    commit

--- app/src/main/java/com/example/howareyou/MainActivity.kt
+++ app/src/main/java/com/example/howareyou/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.howareyou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.howareyou.Model.PostingDTO
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var postingAdapter: PostingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var postingDTOlist : ArrayList<PostingDTO> = arrayListOf()
+        postingDTOlist.add(PostingDTO(0,1,"sample","#HowAreYou?"))
+        postingDTOlist.add(PostingDTO(0,2,"sample","#HowAreYou??"))
+        postingDTOlist.add(PostingDTO(0,3,"sample","#HowAreYou???"))
+        postingDTOlist.add(PostingDTO(0,4,"sample","#HowAreYou????"))
+        postingDTOlist.add(PostingDTO(0,5,"sample","#HowAreYou?????"))
+
+        postingAdapter =PostingAdapter(this,postingDTOlist)
+
+        main_recyclerview.adapter = postingAdapter
+
+        val lm = LinearLayoutManager(this)
+        main_recyclerview.layoutManager = lm
+        main_recyclerview.setHasFixedSize(true)
+
+        // recyclerview 역순출력
+        lm.reverseLayout = true
+        lm.stackFromEnd = true
     }
 }
---
+++ app/src/main/java/com/example/howareyou/Model/PostingDTO.kt
@@ -0,0 +1,8 @@
+package com.example.howareyou.Model
+
+data class PostingDTO(
+    val type : Int,
+    val number : Int,
+    val image : String,
+    val content : String
+)
